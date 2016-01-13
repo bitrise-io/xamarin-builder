8a962c062c766b66932c25580ab10ab785752cb7
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-01-13 17:09:15 +0100

    export archives

--- builder.rb
+++ builder.rb
@@ -1,1 +1,31 @@
 require_relative './analyzer'
+
+class Builder
+  def initialize(project_path, configuration, platform)
+    fail 'Empty path provided' if project_path.to_s == ''
+    fail "File (#{project_path}) not exist" unless File.exist? project_path
+
+    fail 'No configuration provided' if configuration.to_s == ''
+    fail 'No platform provided' if platform.to_s == ''
+
+    @project_path = project_path
+    @configuration = configuration
+    @platform = platform
+  end
+
+  def build
+    analyzer = Analyzer.new()
+    analyzer.analyze(@project_path)
+
+    command = analyzer.build_command(@configuration, @platform)
+
+    puts
+    puts command
+    puts
+    system(command)
+
+    output_hash = analyzer.output_hash(@configuration, @platform)
+    puts
+    puts "output_hash: #{output_hash}"
+  end
+end
