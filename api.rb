6d6ff6d4f3e9ad12af44a004cb56954412181302
olegoid <olegoid91@gmail.com>
2016-04-23 20:25:28 -0300

    Encapsulate API constants into a named set of strings

---
+++ api.rb
@@ -0,0 +1,7 @@
+module Api
+  IOS = 'ios' # Xamarin.iOS projects
+  MAC = 'mac' # Xamarin.Mac projects
+  TVOS = 'tvos' # Xamarin.TVOS projects
+  ANDROID = 'android' # Xamarin.Android projects
+  UITEST = 'uitest' # Xamarin.UITest projects
+end
