1ccadc9c96fc01d5c637221fe4353c222aa6c0e0
Oleg Demchenko <olegoid91@gmail.com>
2016-07-28 12:30:57 -0300

    Touch.Unit related functionality (#34)
    * Add Touch.Unit logic to builder
    
    * Add Touch.Unit related logic to analyzer

--- analyzer.rb
+++ analyzer.rb
@@ -259,6 +259,54 @@ class Analyzer
     [build_commands, errors]
   end
 
+  def nunit_light_test_commands(config, platform, touch_unit_server, logfile)
+    configuration = "#{config}|#{platform}"
+    build_commands = []
+    errors = []
+
+    raise "Touch.Server.exe not found at path: #{touch_unit_server}" unless File.exists?(touch_unit_server)
+
+    @solution[:projects].each do |project|
+      next if project[:tests].nil? || !project[:tests].include?(Tests::NUNIT_LITE)
+
+      test_project = project[:name]
+
+      unless project[:mappings]
+        errors << "#{test_project} not found in solution mappings"
+        errors << project.to_s
+        next
+      end
+
+      project_configuration = project[:mappings][configuration]
+      unless project_configuration
+        errors << "no mapping found for #{test_project} with #{configuration}"
+        errors << project.to_s
+        next
+      end
+
+      project_config = project_configuration.split('|').first
+      unless project_config
+        errors << "#{test_project} configuration #{project_configuration} is invalid"
+        errors << project.to_s
+        next
+      end
+
+      build_commands << mdtool_build_command('build', project_configuration, @solution[:path], project[:name])
+
+      command = [
+          "mono --debug",
+          "\"#{touch_unit_server}\"",
+          "--launchsim",
+          "-autoexit",
+          "-logfile=#{logfile}"
+      ]
+
+      build_commands << command
+    end
+
+    [build_commands, errors]
+  end
+
   def nunit_test_commands(config, platform, options)
     configuration = "#{config}|#{platform}"
     build_commands = []
@@ -704,6 +752,9 @@ class Analyzer
 
       match = line.match(REGEX_PROJECT_REFERENCE_NUNIT_FRAMEWORK)
       (project[:tests] ||= []) << Tests::NUNIT if match != nil
+
+      match = line.match(REGEX_PROJECT_REFERENCE_NUNIT_LITE_FRAMEWORK)
+      (project[:tests] ||= []) << Tests::NUNIT_LITE if match != nil
 
       # Referred projects
       match = line.match(REGEX_PROJECT_PROJECT_REFERENCE_END)
--- builder.rb
+++ builder.rb
@@ -87,7 +87,98 @@ class Builder
     end
   end
 
+  def run_nunit_lite_tests
+    dir = Dir.mktmpdir
+    begin
+      touch_unit_server = get_touch_unit_server(dir)
+
+      logfile = 'tests.log'
+      test_commands, errors = @analyzer.nunit_light_test_commands(@configuration, @platform, touch_unit_server, logfile)
+
+      if test_commands.nil? || test_commands.empty?
+        errors = ['Failed to create test command'] if errors.empty?
+        raise errors.join("\n")
+      end
+
+      app_file = nil
+      test_commands.each do |test_command|
+        puts
+        puts "\e[34m#{test_command}\e[0m"
+        puts
+
+        command = test_command.join(' ')
+        command.sub! '--launchsim', "--launchsim #{app_file}" if command.include? touch_unit_server and !app_file.nil?
+
+        raise 'Test failed' unless system(command)
+
+        if command.include? 'mdtool'
+          @generated_files = @analyzer.collect_generated_files(@configuration, @platform, @project_type_filter)
+          @generated_files.each do |_, project_output|
+            app_file = project_output[:app] if project_output[:api] == Api::IOS and project_output[:app]
+          end
+        end
+      end
+
+      results = process_touch_unit_logs(logfile)
+
+      # remove logs file
+      FileUtils.remove_entry logfile
+
+      raise 'Test failed' if results['Failed'] != '0'
+    rescue => ex
+      error_with_message(ex.inspect.to_s)
+      error_with_message('--- Stack trace: ---')
+      error_with_message(ex.backtrace.to_s)
+      exit(1)
+    ensure
+      FileUtils.remove_entry dir
+    end
+  end
+
   def generated_files
     @generated_files
+  end
+
+  private
+
+  def get_touch_unit_server(directory)
+    # clone Touch.Unit
+    `git clone git@github.com:spouliot/Touch.Unit.git #{directory}`
+    server_project_path = File.join(directory, 'Touch.Server/Touch.Server.csproj')
+
+    # build Touch.Unit server
+    puts `xbuild #{server_project_path}`
+
+    exe_files = []
+    Find.find(File.join(directory, 'Touch.Server/bin/Debug/')) do |path|
+      exe_files << path if path =~ /.*\.exe$/
+    end
+
+    touch_unit_server = exe_files.first
+    raise 'Touch.Server.exe was not found' unless touch_unit_server
+
+    touch_unit_server
+  end
+
+  def process_touch_unit_logs(logs_path)
+    results = Hash.new
+    if File.exist?(logs_path)
+      File.open(logs_path, "r") do |f|
+        f.each_line do |line|
+          puts line
+          if line.start_with?('Tests run')
+            line.gsub (/[a-zA-z]*: [0-9]*/) { |s|
+              s.delete!(' ')
+              test_result = s.split(/:/)
+              results[test_result.first] = test_result.last
+            }
+          end
+        end
+      end
+    else
+      raise 'Cant find test logs file'
+    end
+
+    results
   end
 end
